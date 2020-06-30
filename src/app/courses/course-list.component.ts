9bfc9e40951af683c73c75e773b3be2b7f338846
Gabriela Guida <61994595+gabsguida@users.noreply.github.com>
2020-06-30 14:33:55 -0300

    tratando eventos data binding

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -8,15 +8,39 @@ import { CourseService } from './course.service';
 })
 
 export class CourseListComponent implements OnInit{
-    courses: Course[] = [];
+    
+    filteredCourses: Course[] = [];
+    
+    _courses: Course[] = [];
+    
+    // uso do anderline para indicar que esta variável seja usada somente aqui
+    _filterBy: string;
 
     // chamará o course.service porque está injectable em root
     constructor(private courseService: CourseService){}
 
     ngOnInit(): void {
-        this.courses = this.courseService.retrieveAll();
+        // recebe a lista de cursos pelo courseService
+        this._courses = this.courseService.retrieveAll();
+        
+        // recebe a lista de cursos
+        this.filteredCourses = this._courses;
     }
 
+    // quando digita
+    set filter(value: string) {
+        // valor recebido pelo input
+        this._filterBy = value;
+
+        // irá filtrar o nome do curso da lista pela posição do 1° substring dada no input
+        // os valores devem ficar em minúsculo para que não haja discrepâncias
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    // pegará as alterações
+    get filter(){
+        return this._filterBy;
+    }
 }
 
 /*  
